c97339ede03cf170b22465eff801d15aa066bf2e
luisarita <github@luisarita.com>
2015-04-14 10:14:43 -0600

    Manejo de Actividades via Accesos

---
+++ www/accesos.php
@@ -0,0 +1,14 @@
+<?php 
+require_once('conf.php');
+require_once('connections/conexion.php'); 
+require_once('functions/functions.php');
+require_once('functions/db.php');
+require_once('functions/date.php');
+require_once('functions/text.php');
+require_once('functions/horarios.php');
+require_once('accesos/accesos.php'); 
+
+if (!isset($extended) || !$extended){
+ $r = new accesos();
+ $r->action();
+}
