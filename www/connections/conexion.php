333f862544478120be66da91064b6fe847b68274
luisarita <github@luisarita.com>
2015-06-04 16:00:52 -0600

    Added database connectivity file and configuration

--- www/connections/conexion.php
+++ www/connections/conexion.php
@@ -1,9 +1,7 @@
 <?php
+require_once("../conf.php");
 date_default_timezone_set('America/Tegucigalpa');
 
-$hostname_conexion = "server.local";
-$database_conexion = "smpp";
-$username_conexion = "root";
-$password_conexion = "root";
-$conexion = mysql_connect($hostname_conexion, $username_conexion, $password_conexion,false,128) or trigger_error(mysql_error(),E_USER_ERROR); 
-mysql_select_db($database_conexion) or die(mysql_error());
+
+$conexion = mysql_connect($_CONF['mysql_host'], $_CONF['mysql_user'], $_CONF['mysql_pass'],false,128) or trigger_error(mysql_error(),E_USER_ERROR); 
+mysql_select_db($_CONF['mysql_database']) or die(mysql_error());
